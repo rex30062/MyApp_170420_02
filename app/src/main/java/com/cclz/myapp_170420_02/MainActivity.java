5ae57383125fb05f532559f62b46a0d1c9c638da
Cclz <rex620620>
2017-04-20 11:32:40 +0800

    learn External File Storage

--- app/src/main/java/com/cclz/myapp_170420_02/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170420_02/MainActivity.java
@@ -1,10 +1,15 @@
 package com.cclz.myapp_170420_02;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -27,6 +32,21 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Log.d("MFILE", str);
+    }
 
+    public void click1(View v){
+        File f1= Environment.getExternalStorageDirectory();
+        Log.d("MFILE", "f1: " + f1.toString());
+        File f2=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        Log.d("MFILE", "f2: " + f2.toString());
+        try {
+            FileWriter fw=new FileWriter((f1.toString() + File.separator + "test2.txt"));
+            BufferedWriter bw=new BufferedWriter(fw);
+            bw.write("Hello");
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
