e8374a1263e5a01ecc93f3c9fb5b0d997d58566a
Cclz <rex620620>
2017-04-20 10:43:57 +0800

    Initial commit

---
+++ app/src/main/java/com/cclz/myapp_170420_02/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cclz.myapp_170420_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InputStream is=getResources().openRawResource(R.raw.test1);
+        BufferedReader br=new BufferedReader(new InputStreamReader(is));
+        String str=null;
+        try {
+            str=br.readLine();
+            br.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("MFILE", str);
+
+    }
+}
